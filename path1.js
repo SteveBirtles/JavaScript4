2239d8bece59cf47d16464045f9dbda5e5b3c1bc
Steve Birtles <sbirtles@farnborough.ac.uk>
2019-09-30 23:26:08 +0100

    Add files via upload

--- path1.js
+++ path1.js
@@ -25,18 +25,44 @@ function pageLoad() {
 
     setInterval(addBall, 1000);
 
+    const canvas = document.getElementById('pathCanvas');
+
+    canvas.addEventListener('click', event => {
+        addMarker(event.clientX, event.clientY);
+    }, false);
+
+    canvas.addEventListener('contextmenu', event => {
+        removeMarker();
+        event.preventDefault();
+    }, false);
+
 }
 
 function addBall() {
 
 	let x = markers[0].x;
 	let y = markers[0].y;
-	let r = 20;
+	let r = 30;
 
 	let nextMarker = 1;
 	let progress = 0;
 
 	balls.push({x, y, r, nextMarker, progress});
+
+}
+
+function addMarker(x, y) {
+
+    let lastMarker = markers.pop();
+    markers.push({x, y});
+    markers.push(lastMarker);
+
+}
+
+function removeMarker() {
+
+    if (markers.length <= 2) return;
+    markers.splice(markers.length - 2, 1);
 
 }
 
@@ -62,7 +88,9 @@ function redraw(timestamp) {
     	b.x = markers[n-1].x + (markers[n].x - markers[n-1].x) * b.progress;
     	b.y = markers[n-1].y + (markers[n].y - markers[n-1].y) * b.progress;
 
-        b.progress += frameLength;
+        let d = Math.sqrt(Math.pow( markers[n].x - markers[n-1].x, 2) + Math.pow(markers[n].y - markers[n-1].y, 2));
+
+        b.progress += frameLength * 100/d;
  	    if (b.progress >= 1) {
  			b.nextMarker++;
  			b.progress -= 1;
@@ -70,10 +98,32 @@ function redraw(timestamp) {
 
     }
 
+    while (balls.length > 0) {
+        if (balls[0].nextMarker < markers.length) break;
+        balls.shift();
+    }
+
+    context.strokeStyle = 'blue';
+    context.lineWidth = 5;
+    context.lineCap = 'round';
+    context.setLineDash([5, 10]);
+
+    let lastMarker = null;
+    for (let marker of markers) {
+        if (lastMarker != null) {
+            context.beginPath();
+            context.moveTo(lastMarker.x, lastMarker.y);
+            context.lineTo(marker.x, marker.y);
+            context.stroke();
+        }
+        lastMarker = marker;
+    }
+
     for (let b of balls) {
-       if (b.nextMarker >= markers.length) continue;
        context.drawImage(ballImage, 0,0, ballImage.width, ballImage.height, b.x-b.r, b.y-b.r, b.r*2, b.r*2);
     }
+
+
 
     window.requestAnimationFrame(redraw);
 
